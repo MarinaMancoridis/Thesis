4b93464f92a2c5c9477faac6918159b22154133e
Marina-Mancoridis <mm96@princeton.edu>
2024-01-11 12:27:08 -0500

    added in the two evaluation metrics

--- Thesis_Simulations/evaluation.py
+++ Thesis_Simulations/evaluation.py
@@ -10,10 +10,27 @@ def kl_divergence(count_zero_p, count_one_p, count_zero_q, count_one_q):
 
 
 # this determines how well scientists have determined the correct parameters for each arm in the multi-armed bandit game
-def arm_parameter_score(participants, scientific_record):
-    # return the median score across all participants
-    return 0
+def arm_parameter_score(scientific_record, bins_to_probs):
+    total_deviation = 0
+    num_bins = 0
+    
+    for bin_num in bins_to_probs:
+        recorded_success_rate = scientific_record[bin_num][1] / (scientific_record[bin_num][1] + scientific_record[bin_num][0])
+        
+        total_deviation += abs(recorded_success_rate - bins_to_probs[bin_num])
+        num_bins += 1
+    
+    
+    return total_deviation / num_bins
 
 # this determines how well scientists have reduced the entropy of their published results
-def total_entropy_score(participants, scientific_record):
-    return 0
+# returns the average KL divergence for a bin
+def total_entropy_score(scientific_record, bins_to_probs):
+    total_kl_divergence = 0
+    num_bins = 0
+    
+    for bin_num in bins_to_probs:
+        num_bins += 1
+        total_kl_divergence += kl_divergence(1, 1, scientific_record[bin_num][0], scientific_record[bin_num][1])
+
+    return total_kl_divergence / num_bins
